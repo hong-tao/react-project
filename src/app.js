3053580e13f7b39b7053187365ab3f148e61ed28
hongtao <897203787@qq.com>
2017-12-20 09:39:40 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {Router, Route, hashHistory} from 'react-router'
+import store from './redux/configStore'
+import routes from './router/rootRouter'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}/>
+    </Provider>
+    ,document.getElementById('app'))
---
+++ src/components/products/productsComponent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import DataGrid from '../datagrid/datagridComponent'
+
+export default class ProductsComponent extends React.Component{
+    render(){
+        return <DataGrid></DataGrid>
+    }
+}
